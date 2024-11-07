d7d7ee69ff22c5d2187036e2ca2819e3435bf4ba
Akbar Hossain <hakbar431@gmail.com>
2024-11-08 00:27:20 +0600

    first commit

---
+++ day071124/index.js
@@ -0,0 +1,3 @@
+const student01 = require('./student')
+
+console.log(student01.getName(),student01.getGender());
---
+++ day071124/student.js
@@ -0,0 +1,14 @@
+const getName = () => {
+    return "Akbar Hossain"
+}
+
+const getGender = () => {
+    return 28
+}
+
+// exports.getName = getName
+// exports.getGender = getGender
+module.exports = {
+    getName,
+    getGender
+}
